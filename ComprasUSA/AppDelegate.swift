ca557b4bd04154b084e533ac3b7b7e7bd4c2ad4f
Carlos R Pantoja Caldas <carlosrpcaldas@hotmail.com>
2018-04-22 14:00:02 -0300

    Adicionado tela de imposto de estado

--- ComprasUSA/AppDelegate.swift
+++ ComprasUSA/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let appDefaults: [String: Any] = ["dolar_preference": "3.4", "iof_preference": "1.0"]
+        UserDefaults.standard.register(defaults: appDefaults)
+        
         return true
     }
 
@@ -40,7 +44,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "ComprasUSA")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 
 
 }
+
+
 
